d9897ebbdb1c0af04449bf35c048d1a4ad724d74
r0744382 <r0744382@student.thomasmore.be>
2021-01-20 14:01:21 +0100

    qr code

--- app/Http/Controllers/QrcodeController.php
+++ app/Http/Controllers/QrcodeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use LaravelQRCode\Facades\QRCode;
 
+
 class QrcodeController extends Controller
 {
 
@@ -13,7 +14,7 @@ class QrcodeController extends Controller
     public function make()
     {
         $file = public_path('assets/qr.png');
-        qrcode::text('thomasmore.be')->setOutfile($file)->png();
+        qrcode::text('thomasmore.be')->setOutfile($file)->setSize(10)->png();
         return view('qrcode/qrcode');
 
     }
