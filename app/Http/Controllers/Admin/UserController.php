aceb649a1b00cd36f2df06dfe10f60deb1a4504d
r0744382 <r0744382@student.thomasmore.be>
2021-01-28 16:02:09 +0100

    privileges advanced

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -112,14 +112,33 @@ class UserController extends Controller
             $isAdmin = $request->admin = 0;
         }
 
-        if($request->type == "lokale_admin") {
-            $permissions = "[\"ALERT_CREATE_COMPANY\",\"ALERT_READ_COMPANY\",\"ALERT_DELETE_COMPANY\",\"AUTHENTICATION_CREATE_COMPANY\",\"AUTHENTICATION_READ_COMPANY\",\"AUTHENTICATION_UPDATE_COMPANY\",\"AUTHENTICATION_DELETE_COMPANY\",\"AUTHERIZED_USER_PER_MACHINE_CREATE_COMPANY\",\"AUTHERIZED_USER_PER_MACHINE_READ_COMPANY\",\"AUTHERIZED_USER_PER_MACHINE_DELETE_COMPANY\",\"COMPANY_UPDATE_COMPANY\",\"USER_CREATE_COMPANY\",\"USER_READ_COMPANY\",\"USER_UPDATE_COMPANY\",\"USER_DELETE_COMPANY\",\"USER_THAT_RECEIVE_ALERTS_FROM_VENDING_MACHINE_CREATE_COMPANY\",\"USER_THAT_RECEIVE_ALERTS_FROM_VENDING_MACHINE_READ_COMPANY\",\"USER_THAT_RECEIVE_ALERTS_FROM_VENDING_MACHINE_DELETE_COMPANY\",\"VENDING_MACHINE_CREATE_COMPANY\",\"VENDING_MACHINE_READ_COMPANY\",\"VENDING_MACHINE_UPDATE_COMPANY\",\"VENDING_MACHINE_DELETE_COMPANY\"]";
-        } elseif($request->type == "admin") {
-            $permissions = "[\"ALERT_CREATE\",\"ALERT_READ\",\"ALERT_DELETE\",\"AUTHENTICATION_CREATE\",\"AUTHENTICATION_READ\",\"AUTHENTICATION_UPDATE\",\"AUTHENTICATION_DELETE\",\"AUTHERIZED_USER_PER_MACHINE_CREATE\",\"AUTHERIZED_USER_PER_MACHINE_READ\",\"AUTHERIZED_USER_PER_MACHINE_DELETE\",\"COMPANY_CREATE\",\"COMPANY_READ\",\"COMPANY_UPDATE\",\"COMPANY_DELETE\",\"USER_CREATE\",\"USER_READ\",\"USER_UPDATE\",\"USER_DELETE\",\"USER_THAT_RECEIVE_ALERTS_FROM_VENDING_MACHINE_CREATE\",\"USER_THAT_RECEIVE_ALERTS_FROM_VENDING_MACHINE_READ\",\"USER_THAT_RECEIVE_ALERTS_FROM_VENDING_MACHINE_DELETE\",\"VENDING_MACHINE_CREATE\",\"VENDING_MACHINE_READ\",\"VENDING_MACHINE_UPDATE\",\"VENDING_MACHINE_DELETE\"]";
-        }elseif($request->type == "gebruiker") {
-            $permissions = "[\"AUTHENTICATION_CREATE_COMPANY_OWN\"]";
-        }elseif($request->type == "guest") {
-            $permissions = "[]";
+        if($request->privileges == 2) {
+            $permissions = "";
+
+            for ($x = 1; $x <= 48; $x++) {
+
+                if($request->$x != "") {
+                    if($permissions == "") {
+                        $permissions = "[\"".$request->$x."\"";
+                    } else {
+                        $permissions .= ",\"".$request->$x."\"";
+                    }
+
+                    echo($request->$x . "\n");
+                }
+            }
+            $permissions .= "]";
+        } elseif ($request->privileges == 1) {
+            if($request->type == "lokale_admin") {
+                $permissions = "[\"ALERT_CREATE_COMPANY\",\"ALERT_READ_COMPANY\",\"ALERT_DELETE_COMPANY\",\"AUTHENTICATION_CREATE_COMPANY\",\"AUTHENTICATION_READ_COMPANY\",\"AUTHENTICATION_UPDATE_COMPANY\",\"AUTHENTICATION_DELETE_COMPANY\",\"AUTHERIZED_USER_PER_MACHINE_CREATE_COMPANY\",\"AUTHERIZED_USER_PER_MACHINE_READ_COMPANY\",\"AUTHERIZED_USER_PER_MACHINE_DELETE_COMPANY\",\"COMPANY_UPDATE_COMPANY\",\"USER_CREATE_COMPANY\",\"USER_READ_COMPANY\",\"USER_UPDATE_COMPANY\",\"USER_DELETE_COMPANY\",\"USER_THAT_RECEIVE_ALERTS_FROM_VENDING_MACHINE_CREATE_COMPANY\",\"USER_THAT_RECEIVE_ALERTS_FROM_VENDING_MACHINE_READ_COMPANY\",\"USER_THAT_RECEIVE_ALERTS_FROM_VENDING_MACHINE_DELETE_COMPANY\",\"VENDING_MACHINE_CREATE_COMPANY\",\"VENDING_MACHINE_READ_COMPANY\",\"VENDING_MACHINE_UPDATE_COMPANY\",\"VENDING_MACHINE_DELETE_COMPANY\"]";
+            } elseif($request->type == "admin") {
+                $permissions = "[\"ALERT_CREATE\",\"ALERT_READ\",\"ALERT_DELETE\",\"AUTHENTICATION_CREATE\",\"AUTHENTICATION_READ\",\"AUTHENTICATION_UPDATE\",\"AUTHENTICATION_DELETE\",\"AUTHERIZED_USER_PER_MACHINE_CREATE\",\"AUTHERIZED_USER_PER_MACHINE_READ\",\"AUTHERIZED_USER_PER_MACHINE_DELETE\",\"COMPANY_CREATE\",\"COMPANY_READ\",\"COMPANY_UPDATE\",\"COMPANY_DELETE\",\"USER_CREATE\",\"USER_READ\",\"USER_UPDATE\",\"USER_DELETE\",\"USER_THAT_RECEIVE_ALERTS_FROM_VENDING_MACHINE_CREATE\",\"USER_THAT_RECEIVE_ALERTS_FROM_VENDING_MACHINE_READ\",\"USER_THAT_RECEIVE_ALERTS_FROM_VENDING_MACHINE_DELETE\",\"VENDING_MACHINE_CREATE\",\"VENDING_MACHINE_READ\",\"VENDING_MACHINE_UPDATE\",\"VENDING_MACHINE_DELETE\"]";
+            }elseif($request->type == "gebruiker") {
+                $permissions = "[\"AUTHENTICATION_CREATE_COMPANY_OWN\"]";
+            }elseif($request->type == "guest") {
+                $permissions = "[]";
+            }
+
         }
 
 
@@ -209,12 +228,18 @@ class UserController extends Controller
 
 
         if($request->privileges == 2) {
-            $permissions = "[\"";
+            $permissions = "";
 
             for ($x = 1; $x <= 48; $x++) {
-                $rechten = $request->permission.$x;
-                if($rechten != "") {
-                    $permissions .= "ALERT_CREATE\",";
+
+                if($request->$x != "") {
+                    if($permissions == "") {
+                        $permissions = "[\"".$request->$x."\"";
+                    } else {
+                        $permissions .= ",\"".$request->$x."\"";
+                    }
+
+                    echo($request->$x . "\n");
                 }
             }
             $permissions .= "]";
